c7a5f9ab55a1fddc9cd74201586666e8a5335f42
timur.shevtsov <timur.shevtsov@sptovarov.dev>
2022-08-29 22:27:31 +0200

    fix

--- src/Classes/SitemapFile.php
+++ src/Classes/SitemapFile.php
@@ -2,13 +2,14 @@
 
 namespace Sitemap\Classes;
 
+use Illuminate\Support\Collection;
 use Sitemap\Interfaces\SitemapFactoryInterface;
 
 class SitemapFile implements SitemapFactoryInterface
 {
     protected array $sitemap_data;
 
-    public function __construct(array $sitemap_data)
+    public function __construct(array|Collection $sitemap_data)
     {
         $this->sitemap_data = $sitemap_data;
     }
